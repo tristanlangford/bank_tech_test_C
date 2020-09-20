6e69730519cc413a5d877dafef0cd2d05decf5a4
Tristan Langford <tristanlangford@hotmail.com>
2020-09-20 17:35:01 +0100

    can deposit

---
+++ Bank_Tech_Test_C/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Bank_Tech_Test_C
+{
+    public class Account
+    {
+        public double balance
+        { get; private set; }
+
+        public Account()
+        {
+            balance = 0;
+        }
+
+        public void Deposit(double value)
+        {
+            balance += value;
+        }
+    }
+}
--- Bank_Tech_Test_C_Unit_Tests/UnitTest1.cs
+++ Bank_Tech_Test_C_Unit_Tests/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using Bank_Tech_Test_C;
 
 namespace Bank_Tech_Test_C_Unit_Tests
 {
-    public class UnitTest1
+    public class AccountTest
     {
-        [Fact]
-        public void Test1()
-        {
+        public Account account;
 
+        public AccountTest()
+        {
+            account = new Account();
         }
+
+        [Fact]
+        public void BalanceStartsAt0()
+        {
+            Assert.Equal(0, account.balance);
+        }
+
+        [Fact]
+        public void DepositIncreasesBalance()
+        {
+            account.Deposit(30);
+            Assert.Equal(30, account.balance);
+        }
+
     }
 }
