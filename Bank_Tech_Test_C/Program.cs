90c13870f9444a43a73567590a3fd06f62e237ab
Tristan Langford <tristanlangford@hotmail.com>
2020-09-20 17:07:55 +0100

    first commit

---
+++ Bank_Tech_Test_C/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bank_Tech_Test_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Bank_Tech_Test_C_Unit_Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Bank_Tech_Test_C_Unit_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
