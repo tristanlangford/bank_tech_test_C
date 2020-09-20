5391cbd9718b7eeb7a4a6aa13db961dd7d3d9d63
Tristan Langford <tristanlangford@hotmail.com>
2020-09-20 18:33:40 +0100

    interaction stores old & new balance

---
+++ Bank_Tech_Test_C/Interaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Bank_Tech_Test_C
+{
+    public class Interaction
+    {
+        public double OldBalance;
+        public double NewBalance;
+        public DateTime Date;
+
+        public Interaction(double value, double oldBalance)
+        {
+            OldBalance = oldBalance;
+            NewBalance = OldBalance + value;
+        }
+    }
+}
---
+++ Bank_Tech_Test_C_Unit_Tests/InteractionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xunit;
+using Bank_Tech_Test_C;
+
+namespace Bank_Tech_Test_C_Unit_Tests
+{
+    public class InteractionTest
+    {
+        public Interaction interaction;
+        public InteractionTest()
+        {
+            interaction = new Interaction(30, 10);
+        }
+
+        [Fact]
+        public void StoresOldBalance()
+        {
+            Assert.Equal(10, interaction.OldBalance);
+        }
+
+        [Fact]
+        public void SetsNewBalance()
+        {
+            Assert.Equal(40, interaction.NewBalance);
+        }
+
+    }
+}
