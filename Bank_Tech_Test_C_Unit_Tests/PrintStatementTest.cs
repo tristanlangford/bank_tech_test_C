b3afd501ebf7c2ad754c30b418230dee8122c2bc
Tristan Langford <tristanlangford@hotmail.com>
2020-09-22 16:03:05 +0100

    complete

--- Bank_Tech_Test_C_Unit_Tests/PrintStatementTest.cs
+++ Bank_Tech_Test_C_Unit_Tests/PrintStatementTest.cs
@@ -14,7 +14,7 @@ namespace Bank_Tech_Test_C_Unit_Tests
 
         public PrintStatementTest()
         {
-            MockInteraction1 = new Interaction(500, 0, () => new DateTime(2020, 09, 22));
+            MockInteraction1 = new Interaction(500, 0, () => new DateTime(2018, 04, 22));
             MockInteraction2 = new Interaction(-500, 1000, () => new DateTime(2020, 09, 22));
             history = new List<Interaction> { MockInteraction1, MockInteraction2 };
         }
@@ -28,7 +28,7 @@ namespace Bank_Tech_Test_C_Unit_Tests
         [Fact]
         public void PrintsSingleHistory()
         {
-            Assert.Contains("22/09/2020 || || 500.00 || 500.00", PrintStatement.Print(history));
+            Assert.Contains("22/04/2018 || || 500.00 || 500.00", PrintStatement.Print(history));
         }
 
         [Fact]
@@ -40,7 +40,7 @@ namespace Bank_Tech_Test_C_Unit_Tests
         [Fact]
         public void DealsWithList()
         {
-            Assert.Contains("22/09/2020 || || 500.00 || 500.00", PrintStatement.Print(history));
+            Assert.Contains("22/04/2018 || || 500.00 || 500.00", PrintStatement.Print(history));
             Assert.Contains("22/09/2020 || 500.00 || || 500.00", PrintStatement.Print(history));
         }
 
